2e6721b4afead5da46446f455f111ce9e5a0722b
JojiKai_LapTop <whatever1668@gmail.com>
2025-07-30 17:46:31 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="logo" />
+      </div>
+      <ul>
+        <li>Menu</li>
+        <li>Location</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <button>login</button>
+    </nav>
+  );
+}
+
+export default App;
